9988ce5b4d6fcbba248a76652419aada03005f1e
Nicolas Kruchten <nicolas@plot.ly>
2019-02-02 13:31:33 -0500

    setup setup.py

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This is alpha-quality software, under active development and available for demonstration purposes only!
 
-[![nbviewer badge](https://raw.githubusercontent.com/jupyter/design/master/logos/Badges/nbviewer_badge.svg)](https://nbviewer.jupyter.org/github/plotly/plotly_express/blob/master/gallery.ipynb)
+[![nbviewer badge](https://img.shields.io/badge/render-nbviewer-orange.svg)](https://nbviewer.jupyter.org/github/plotly/plotly_express/blob/master/gallery.ipynb)
 
 [![binder badge](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/plotly/plotly_express/master?filepath=gallery.ipynb)
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from os import path
+
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="plotly_express",  # Required
+    version="0.1a1",  # Required
+    description="Plotly Express: a high level wrapper for Plotly.py",  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    url="https://github.com/plotly/plotly_express",  # Optional
+    author="Nicolas Kruchten",  # Optional
+    author_email="nicolas@plot.ly",  # Optional
+    classifiers=[  # Optional
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=find_packages(),  # Required
+    package_data={"plotly_express": ["data/*.csv.xz"]},
+    install_requires=["pandas>=0.20.0", "plotly>=3.6.0"],  # Optional
+)
