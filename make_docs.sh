22cfd25f4bc647700c668378db72a8c0f430646b
Nicolas Kruchten <nicolas@datacratic.com>
2019-03-19 23:06:42 -0400

    documentation

--- make_docs.sh
+++ make_docs.sh
@@ -2,4 +2,5 @@
 
 set -e
 jupyter nbconvert gallery.ipynb --execute --output-dir=docs --output=index --template=docs/nb.tpl
+jupyter nbconvert walkthrough.ipynb --execute --output-dir=docs --template=docs/nb.tpl
 pdoc plotly_express --html --html-dir=docs --overwrite --template-dir=docs/templates
