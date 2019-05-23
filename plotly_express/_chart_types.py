8818afcba408c21b8ac5286bb7299309ef7a3b29
Nicolas Kruchten <nicolas@plot.ly>
2019-05-23 08:45:24 -0400

    density_heatmap and fixes to density_contour

--- plotly_express/_chart_types.py
+++ plotly_express/_chart_types.py
@@ -58,6 +58,7 @@ def density_contour(
     data_frame,
     x=None,
     y=None,
+    z=None,
     color=None,
     facet_row=None,
     facet_col=None,
@@ -77,6 +78,10 @@ def density_contour(
     log_y=False,
     range_x=None,
     range_y=None,
+    histfunc=None,
+    histnorm=None,
+    nbinsx=None,
+    nbinsy=None,
     title=None,
     template=None,
     width=None,
@@ -84,16 +89,78 @@ def density_contour(
 ):
     """
     In a density contour plot, rows of `data_frame` are grouped together into contour marks to \
-    visualize the density of their distribution in 2D space.
+    visualize the 2D distribution of an aggregate function `histfunc` (e.g. the count or sum) \
+    of the value `z`.
     """
     return make_figure(
         args=locals(),
         constructor=go.Histogram2dContour,
-        trace_patch=dict(contours=dict(coloring="none")),
+        trace_patch=dict(
+            contours=dict(coloring="none"),
+            histfunc=histfunc,
+            histnorm=histnorm,
+            nbinsx=nbinsx,
+            nbinsy=nbinsy,
+            xbingroup="x",
+            ybingroup="y",
+        ),
     )
 
 
 density_contour.__doc__ = make_docstring(density_contour)
+
+
+def density_heatmap(
+    data_frame,
+    x=None,
+    y=None,
+    z=None,
+    facet_row=None,
+    facet_col=None,
+    hover_name=None,
+    hover_data=None,
+    animation_frame=None,
+    animation_group=None,
+    category_orders={},
+    labels={},
+    color_continuous_scale=None,
+    color_continuous_midpoint=None,
+    marginal_x=None,
+    marginal_y=None,
+    opacity=None,
+    log_x=False,
+    log_y=False,
+    range_x=None,
+    range_y=None,
+    histfunc=None,
+    histnorm=None,
+    nbinsx=None,
+    nbinsy=None,
+    title=None,
+    template=None,
+    width=None,
+    height=None,
+):
+    """
+    In a density heatmap, rows of `data_frame` are grouped together into colored \
+    rectangular tiles to visualize the 2D distribution of an aggregate function \
+    `histfunc` (e.g. the count or sum) of the value `z`.
+    """
+    return make_figure(
+        args=locals(),
+        constructor=go.Histogram2d,
+        trace_patch=dict(
+            histfunc=histfunc,
+            histnorm=histnorm,
+            nbinsx=nbinsx,
+            nbinsy=nbinsy,
+            xbingroup="x",
+            ybingroup="y",
+        ),
+    )
+
+
+density_heatmap.__doc__ = make_docstring(density_heatmap)
 
 
 def line(
@@ -164,7 +231,6 @@ def area(
     range_x=None,
     range_y=None,
     line_shape=None,
-    render_mode="auto",
     title=None,
     template=None,
     width=None,
@@ -268,7 +334,8 @@ def histogram(
 ):
     """
     In a histogram, rows of `data_frame` are grouped together into a rectangular mark to \
-    visualize some aggregate quantity like count or sum.
+    visualize the 1D distribution of an aggregate function `histfunc` (e.g. the count or sum) \
+    of the value `y` (or `x` if `orientation` is `'h'`).
     """
     return make_figure(
         args=locals(),
@@ -278,8 +345,9 @@ def histogram(
             histnorm=histnorm,
             histfunc=histfunc,
             nbinsx=nbins if orientation == "v" else None,
-            nbinsy=nbins if orientation == "h" else None,
+            nbinsy=None if orientation == "v" else nbins,
             cumulative=dict(enabled=cumulative),
+            bingroup="x" if orientation == "v" else "y",
         ),
         layout_patch=dict(barmode=barmode, barnorm=barnorm),
     )
--- plotly_express/_core.py
+++ plotly_express/_core.py
@@ -3,7 +3,8 @@ from plotly.offline import init_notebook_mode, iplot
 import plotly.io as pio
 from collections import namedtuple, OrderedDict
 from .colors import qualitative, sequential
-import math, pandas
+import math
+import pandas
 
 
 class PxDefaults(object):
@@ -88,8 +89,9 @@ def get_label(args, column):
 def get_decorated_label(args, column, role):
     label = get_label(args, column)
     if "histfunc" in args and (
-        (role == "x" and args["orientation"] == "h")
-        or (role == "y" and args["orientation"] == "v")
+        (role == "x" and "orientation" in args and args["orientation"] == "h")
+        or (role == "y" and "orientation" in args and args["orientation"] == "v")
+        or (role == "z")
     ):
         if label:
             return "%s of %s" % (args["histfunc"] or "count", label)
@@ -171,8 +173,13 @@ def make_trace_kwargs(args, trace_spec, g, mapping_labels, sizeref):
                 mapping_labels["%{xaxis.title.text}"] = "%{x}"
                 mapping_labels["%{yaxis.title.text}"] = "%{y}"
 
-        elif v is not None or (
-            trace_spec.constructor == go.Histogram and k in ["x", "y"]
+        elif (
+            v is not None
+            or (trace_spec.constructor == go.Histogram and k in ["x", "y"])
+            or (
+                trace_spec.constructor in [go.Histogram2d, go.Histogram2dContour]
+                and k == "z"
+            )
         ):
             if k == "size":
                 if "marker" not in result:
@@ -222,12 +229,20 @@ def make_trace_kwargs(args, trace_spec, g, mapping_labels, sizeref):
                     result[error_xy] = {}
                 result[error_xy][arr] = g[v]
             elif k == "hover_name":
-                if trace_spec.constructor not in [go.Histogram, go.Histogram2dContour]:
+                if trace_spec.constructor not in [
+                    go.Histogram,
+                    go.Histogram2d,
+                    go.Histogram2dContour,
+                ]:
                     result["hovertext"] = g[v]
                     if hover_header == "":
                         hover_header = "<b>%{hovertext}</b><br><br>"
             elif k == "hover_data":
-                if trace_spec.constructor not in [go.Histogram, go.Histogram2dContour]:
+                if trace_spec.constructor not in [
+                    go.Histogram,
+                    go.Histogram2d,
+                    go.Histogram2dContour,
+                ]:
                     result["customdata"] = g[v].values
                     for i, col in enumerate(v):
                         v_label_col = get_decorated_label(args, col, None)
@@ -235,7 +250,7 @@ def make_trace_kwargs(args, trace_spec, g, mapping_labels, sizeref):
             elif k == "color":
                 if trace_spec.constructor == go.Choropleth:
                     result["z"] = g[v]
-                    result["z"]["coloraxis"] = "coloraxis1"
+                    result["coloraxis"] = "coloraxis1"
                     mapping_labels[v_label] = "%{z}"
                 else:
                     colorable = "marker"
@@ -255,7 +270,7 @@ def make_trace_kwargs(args, trace_spec, g, mapping_labels, sizeref):
                 if v:
                     result[k] = g[v]
                 mapping_labels[v_label] = "%%{%s}" % k
-    if trace_spec.constructor not in [go.Histogram2dContour, go.Parcoords, go.Parcats]:
+    if trace_spec.constructor not in [go.Parcoords, go.Parcats]:
         hover_lines = [k + "=" + v for k, v in mapping_labels.items()]
         result["hovertemplate"] = hover_header + "<br>".join(hover_lines)
     return result, fit_results
@@ -542,7 +557,7 @@ def make_trace_spec(args, constructor, attrs, trace_patch):
                 trace_spec = TraceSpec(
                     constructor=go.Histogram,
                     attrs=[letter],
-                    trace_patch=dict(opacity=0.5, **axis_map),
+                    trace_patch=dict(opacity=0.5, bingroup=letter, **axis_map),
                 )
             elif args["marginal_" + letter] == "violin":
                 trace_spec = TraceSpec(
@@ -571,10 +586,10 @@ def make_trace_spec(args, constructor, attrs, trace_patch):
                         **axis_map
                     ),
                 )
-            if "color" in attrs:
+            if "color" in attrs or "color" not in args:
                 if "marker" not in trace_spec.trace_patch:
                     trace_spec.trace_patch["marker"] = dict()
-                first_default_color = args["color_discrete_sequence"][0]
+                first_default_color = args["color_continuous_scale"][0]
                 trace_spec.trace_patch["marker"]["color"] = first_default_color
             result.append(trace_spec)
     if "trendline" in args and args["trendline"]:
@@ -702,6 +717,11 @@ def infer_config(args, constructor, trace_patch):
         grouped_attrs.append("marker.symbol")
 
     trace_patch = trace_patch.copy()
+
+    if constructor == go.Histogram2d:
+        show_colorbar = True
+        trace_patch["coloraxis"] = "coloraxis1"
+
     if "opacity" in args:
         if args["opacity"] is None:
             if "barmode" in args and args["barmode"] == "overlay":
@@ -797,8 +817,8 @@ def make_figure(args, constructor, trace_patch={}, layout_patch={}):
         trace_names = trace_names_by_frame[frame_name]
 
         for trace_spec in trace_specs:
-            constructor = trace_spec.constructor
-            if constructor in [go.Scatter, go.Scatterpolar]:
+            constructor_to_use = trace_spec.constructor
+            if constructor_to_use in [go.Scatter, go.Scatterpolar]:
                 if "render_mode" in args and (
                     args["render_mode"] == "webgl"
                     or (
@@ -807,11 +827,18 @@ def make_figure(args, constructor, trace_patch={}, layout_patch={}):
                         and args["animation_frame"] is None
                     )
                 ):
-                    constructor = (
-                        go.Scattergl if constructor == go.Scatter else go.Scatterpolargl
+                    constructor_to_use = (
+                        go.Scattergl
+                        if constructor_to_use == go.Scatter
+                        else go.Scatterpolargl
                     )
-            trace = trace_spec.constructor(name=trace_name)
-            if trace_spec.constructor not in [go.Parcats, go.Parcoords, go.Choropleth]:
+            trace = constructor_to_use(name=trace_name)
+            if trace_spec.constructor not in [
+                go.Parcats,
+                go.Parcoords,
+                go.Choropleth,
+                go.Histogram2d,
+            ]:
                 trace.update(
                     legendgroup=trace_name,
                     showlegend=(trace_name != "" and trace_name not in trace_names),
@@ -870,14 +897,18 @@ def make_figure(args, constructor, trace_patch={}, layout_patch={}):
         )
     layout_patch = layout_patch.copy()
     if show_colorbar:
+        if "color" in args:
+            colorvar = "color"
+        elif constructor == go.Histogram2d:
+            colorvar = "z"
         d = len(args["color_continuous_scale"]) - 1
         layout_patch["coloraxis1"] = dict(
-            colorbar=dict(title=get_decorated_label(args, args["color"], "color")),
             colorscale=[
                 [(1.0 * i) / (1.0 * d), x]
                 for i, x in enumerate(args["color_continuous_scale"])
             ],
             cmid=args["color_continuous_midpoint"],
+            colorbar=dict(title=get_decorated_label(args, args[colorvar], colorvar)),
         )
     for v in ["title", "height", "width", "template"]:
         if args[v]:
