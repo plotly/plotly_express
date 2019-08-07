3bf7233cddbdfea84618aa1ffe4030d0b9bf39ea
Nicolas Kruchten <nicolas@plot.ly>
2019-08-07 12:05:31 -0400

    bump version dependency

--- plotly_express/__init__.py
+++ plotly_express/__init__.py
@@ -1,8 +1,7 @@
 """
-`plotly_express` is now an alias to `plotly.express`
+`plotly_express` is now an alias for `plotly.express`
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"  # sync with setup.py!
 
 from plotly.express import *
-from plotly.express import line_3d
